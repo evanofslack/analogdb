f5c02217db3face898a8471f4b38bcea8303e808
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-12 00:18:41 -0500

    scrape analog pictures

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,5 +1,11 @@
 import praw
+import requests
+from PIL import Image
 
 reddit = praw.Reddit("bot_1")
 
-print(reddit.read_only)
+for submission in reddit.subreddit("analog").top(limit=5):
+    if not submission.is_self:
+        pic = requests.get(submission.url, stream=True)
+        img = Image.open(pic.raw)
+        img.show()
