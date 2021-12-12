5a941421f8495e6790bb526d39333e74804b4fe5
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-12 17:51:22 -0500

    refactor into functions

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,11 +1,41 @@
+import base64
+from io import BytesIO
+from typing import List, Tuple
+
 import praw
 import requests
 from PIL import Image
 
-reddit = praw.Reddit("bot_1")
 
-for submission in reddit.subreddit("analog").top(limit=5):
-    if not submission.is_self:
-        pic = requests.get(submission.url, stream=True)
-        img = Image.open(pic.raw)
-        img.show()
+def to_base64(img: Image) -> str:
+    with BytesIO() as buffered:
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue())
+        return img_str
+
+
+def to_image(url: str) -> Image:
+    pic = requests.get(url, stream=True)
+    img = Image.open(pic.raw)
+    # img.show()
+    return img
+
+
+def get_pics() -> Tuple[List[str], List[str]]:
+    reddit = praw.Reddit("bot_1")
+    urls = []
+    blob = []
+
+    for submission in reddit.subreddit("analog").hot(limit=5):
+        if not submission.is_self:
+            urls.append(submission.url)
+            img = to_image(submission.url)
+            base64 = to_base64(img)
+            blob.append(base64)
+    return urls, blob
+
+
+if __name__ == "__main__":
+    urls, blob = get_pics()
+    for url in urls:
+        print(url)
