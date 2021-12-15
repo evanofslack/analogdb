f633e24943e8b279b827c58cd9a9ba77efb9dc18
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-14 23:28:52 -0500

    establish db communication and read sucessfully

--- api/main.go
+++ api/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	db, err := sql.Open("sqlite3", parent_dir()+"/test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err == nil {
+		fmt.Println("Connection Established")
+	} else {
+		log.Fatal(err)
+	}
+
+	var (
+		id  int
+		url string
+	)
+
+	rows, err := db.Query("SELECT id, url from pictures WHERE id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, url)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func parent_dir() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	parent := filepath.Dir(wd)
+	return parent
 }
