1fac47eb42b99d63d8582d3ff211acaf80e5582b
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-14 23:28:18 -0500

    create db in parent directory

--- scraper/main.py
+++ scraper/main.py
@@ -1,19 +1,17 @@
+import os
+
 from database import create_connection, create_picture, create_table
 from scrape import get_pics
 
+db_file = os.path.dirname(os.getcwd()) + "/test.db"
 create_picture_table = "CREATE TABLE IF NOT EXISTS pictures (id integer PRIMARY KEY, url TEXT NOT NULL, raw TEXT NOT NULL)"
-db_file = "test.db"
 
 
 def main():
     conn = create_connection(db_file)
-    if conn is not None:
-        create_table(conn, create_picture_table)
-    else:
-        print("Could not connect to database")
+    create_table(conn, create_picture_table)
 
     pic_data = get_pics()
-    # print(pic_data)
 
     for data in pic_data:
         create_picture(conn, data)
