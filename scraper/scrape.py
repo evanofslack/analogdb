2c9ae2fef3c0f15204807c84a9d5010f1dcbce2a
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-16 01:26:48 -0500

    fix gallery url

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -30,47 +30,48 @@ def to_image(url: str) -> Image:
     return img
 
 
-def handle_image(s: praw.reddit.Submission) -> str:
-    return s.url
-
-
 def handle_gallery(s: praw.reddit.Submission) -> str:
     """
     Return just the first image of the gallery
 
     """
+    print("\nHandling Gallery")
     for item in sorted(s.gallery_data["items"], key=lambda x: x["id"]):
         media_id = item["media_id"]
         meta = s.media_metadata[media_id]
         if meta["e"] == "Image":
             source = meta["s"]
-            return source
+            return source["u"]
 
 
 def get_url(s: praw.reddit.Submission) -> str:
-    if hasattr(s, "is_picture"):
-        if s.is_picture:
-            return handle_image(s)
+
     if hasattr(s, "is_gallery"):
         if s.is_gallery:
             return handle_gallery(s)
+    else:
+        return s.url
+
+
+def full_permalink(path: str) -> str:
+    return "https://www.reddit.com" + path
 
 
 def get_pics() -> List[AnalogData]:
     reddit = praw.Reddit("bot_1")
     pic_data: List[AnalogData] = []
     submissions: List[praw.reddit.Submission] = [
-        s for s in reddit.subreddit("analog").hot(limit=5) if not s.is_self
+        s for s in reddit.subreddit("analog").hot(limit=20) if not s.is_self
     ]
 
     for s in submissions:
         new_pic = AnalogData(
             title=s.title,
             url=get_url(s),
-            permalink=s.permalink,
+            permalink=full_permalink(s.permalink),
             nsfw=s.over_18,
         )
-        print(new_pic)
+        print("\n", new_pic)
         pic_data.append(new_pic)
 
     return pic_data
@@ -78,14 +79,3 @@ def get_pics() -> List[AnalogData]:
 
 if __name__ == "__main__":
     get_pics()
-
-    # reddit = praw.Reddit("bot_1")
-    # url = "https://www.reddit.com/gallery/rhdv1w"
-    # submission = reddit.submission(url=url)
-
-    # print(submission.gallery_data["items"])
-    # print("\n")
-    # print(submission.media_metadata.keys())
-    # meta = submission.media_metadata.pop()
-    # if meta["e"] == "Image":
-    #     print(meta["s"])
