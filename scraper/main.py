81f8a1130b01ae60bb99f47eeb83d9a5fd0240ed
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-17 01:35:41 -0500

    create new table updated schema

--- scraper/main.py
+++ scraper/main.py
@@ -1,27 +1,34 @@
+import dataclasses
 import os
 
 from database import create_connection, create_picture, create_table
 from scrape import get_pics
 
 db_file = os.path.dirname(os.getcwd()) + "/test.db"
-create_picture_table = "CREATE TABLE IF NOT EXISTS pictures (id integer PRIMARY KEY, url TEXT NOT NULL, raw TEXT NOT NULL)"
+create_picture_table = """CREATE TABLE IF NOT EXISTS pictures (
+                            id integer PRIMARY KEY, 
+                            title text, 
+                            url text NOT NULL, 
+                            permalink text,
+                            score integer,
+                            nsfw integer,
+                            time text
+                            );"""
 
 
 def main():
     conn = create_connection(db_file)
     create_table(conn, create_picture_table)
 
-    pic_data = get_pics()
-
-    for data in pic_data:
-        create_picture(conn, data)
-
-    c = conn.cursor()
-    for row in c.execute("SELECT * FROM pictures"):
-        print(row)
-
+    for data in get_pics():
+        create_picture(conn, dataclasses.astuple(data))
     conn.close()
 
 
 if __name__ == "__main__":
     main()
+
+    conn = create_connection(db_file)
+    c = conn.cursor()
+    for row in c.execute("SELECT * FROM pictures"):
+        print(row)
