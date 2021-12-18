fa7f41b2bdf2dfdd24d3976f04f768fe6b674110
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-18 15:58:42 -0500

    add http route to get latest url

--- api/main.go
+++ api/main.go
@@ -3,19 +3,41 @@ package main
 import (
 	"go-reddit/models"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+
 	db_path := parent_dir() + "/test.db"
 	err := models.InitDB(db_path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	models.AllPosts()
+	// models.AllPosts()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", helloWorld)
+	r.Get("/latest", getLatest)
+	http.ListenAndServe(":3000", r)
+}
+
+func getLatest(w http.ResponseWriter, r *http.Request) {
+	latest, err := models.LatestPost()
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(latest.Url))
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
 }
 
 func parent_dir() string {
