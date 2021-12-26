24f3c171204d758630185e7ebd2d6f8979620e06
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-25 19:10:37 -0500

    Update table schema

--- postgres.py
+++ postgres.py
@@ -1,24 +1,45 @@
-import psycopg2
 import os
 
+import psycopg2
 
-def create_connection():
+
+def create_connection(test: bool = False):
     connection = None
     try:
-        DATABASE_URL = os.environ.get("DATABASE_URL")
-        connection = psycopg2.connect(DATABASE_URL)
-        # connection = psycopg2.connect(
-        #    host="localhost", database="Test", user="evan", password="dark"
-        # )
+        if not test:
+            connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+        elif test:
+            connection = psycopg2.connect(
+                host=os.environ.get("DBHOST"),
+                database=os.environ.get("DBNAME"),
+                user=os.environ.get("DBUSER"),
+                password=os.environ.get("DBPASSWORD"),
+            )
+        else:
+            raise Exception("Must set database init")
         return connection
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
 
-def create_table(connection, command: str):
+def create_table(connection):
+
+    create_picture_table = """CREATE TABLE IF NOT EXISTS pictures (
+                            id SERIAL PRIMARY KEY, 
+                            url text NOT NULL UNIQUE, 
+                            title text, 
+                            author text,
+                            permalink text,
+                            score integer,
+                            nsfw boolean,
+                            greyscale boolean,
+                            time text,
+                            width integer,
+                            height integer
+                            );"""
     try:
         c = connection.cursor()
-        c.execute(command)
+        c.execute(create_picture_table)
         connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -28,7 +49,12 @@ def create_picture(conn, data: tuple):
     try:
         c = conn.cursor()
         c.execute(
-            "INSERT INTO pictures(title, url, permalink, score, nsfw, time) VALUES (%s, %s, %s, %s, %s, %s)",
+            """
+            INSERT 
+            INTO pictures(url, title, author, permalink, score, nsfw, greyscale, time, width, height) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+            ON CONFLICT (url) DO NOTHING
+            """,
             data,
         )
         conn.commit()
