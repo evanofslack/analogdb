a62165e8859b103808e5ca78191e9ba52eac93b5
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-25 19:11:10 -0500

    ability to test with local DB

--- main.py
+++ main.py
@@ -3,30 +3,18 @@ import dataclasses
 from postgres import create_connection, create_picture, create_table
 from scrape import get_pics
 
-create_picture_table = """CREATE TABLE IF NOT EXISTS pictures (
-                            id SERIAL PRIMARY KEY, 
-                            url text NOT NULL, 
-                            title text, 
-                            permalink text,
-                            score integer,
-                            nsfw boolean,
-                            time text
-                            );"""
-
 
 def main():
-    conn = create_connection()
-    create_table(conn, create_picture_table)
+    conn = create_connection(test=True)
+    create_table(conn)
 
     for data in get_pics():
         create_picture(conn, dataclasses.astuple(data))
     conn.close()
 
 
-if __name__ == "__main__":
-    main()
-
-    conn = create_connection()
+def get_all():
+    conn = create_connection(test=True)
     c = conn.cursor()
     c.execute("SELECT * FROM pictures")
     row = c.fetchone()
@@ -34,3 +22,7 @@ if __name__ == "__main__":
     while row is not None:
         print(row)
         row = c.fetchone()
+
+
+if __name__ == "__main__":
+    main()
