aa9d2427a01356c1fe304d3feadb1ecbe3449f9a
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-01-02 19:39:09 -0500

    update javascript query selector

--- api/static/script.js
+++ api/static/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var coll = document.getElementsByClassName("example-block");
+    var examples = document.querySelectorAll("div.example-block");
     var i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
+    for (i = 0; i < examples.length; i++) {
+        examples[i].addEventListener("click", function () {
             this.classList.toggle("active");
             var content = this.nextElementSibling;
             if (content.style.display === "block") {
