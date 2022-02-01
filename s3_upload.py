759f44e59bdf7af4dc079c93aeeb6526b13112db
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-01-31 22:20:02 -0500

    add s3 upload function

--- s3_upload.py
+++ s3_upload.py
@@ -1,34 +1,57 @@
+from os import getenv
+from typing import List
+
 import boto3
+import boto3.session
 import requests
 
-# Uses the creds in ~/.aws/credentials
-s3 = boto3.resource("s3")
-bucket = "analog-photos"
-url = "https://preview.redd.it/kjvwj3vfyue81.jpg?width=3732&format=pjpg&auto=webp&s=3354c8e44cbc470f53efd25bf6bda87fe2b901e6"
-filename = "test.jpg"
-# s3_image_filename = 'test_s3_image.png'
-# internet_image_url = 'https://docs.python.org/3.7/_static/py.png'
+
+def init_s3():
+    boto_kwargs = {
+        "aws_access_key_id": getenv("AWS_ACCESS_KEY_ID"),
+        "aws_secret_access_key": getenv("AWS_SECRET_ACCESS_KEY"),
+        "region_name": getenv("AWS_REGION"),
+    }
+    my_session = boto3.session.Session(**boto_kwargs)
+    s3 = my_session.resource("s3")
+    return s3
 
 
-# Do this as a quick and easy check to make sure your S3 access is OK
-for b in s3.buckets.all():
-    print(b)
+def s3_upload(bucket: str, urls: List[str], filenames: List[str]):
+    s3 = init_s3()
+    assert bucket in [b.name for b in s3.buckets.all()]
 
-req = requests.get(url, stream=True)
-file_obj = req.raw
-req_data = file_obj.read()
-s3.Bucket(bucket).put_object(
-    Key=filename,
-    Body=req_data,
-    ContentType="image/jpg",
-)
-print(f"success, uploaded {filename} to {bucket}")
+    viable_content = {
+        "image/png": ".png",
+        "image/jpeg": ".jpeg",
+        "image/jpg": ".jpg",
+        "image/gif": ".gif",
+    }
 
-# Given an Internet-accessible URL, download the image and upload it to S3,
-# without needing to persist the image to disk locally
-# req_for_image = requests.get(internet_image_url, stream=True)
-# file_object_from_req = req_for_image.raw
-# req_data = file_object_from_req.read()
+    for url, filename in zip(urls, filenames):
 
-# # Do the actual upload to s3
-# s3.Bucket(bucket_name_to_upload_image_to).put_object(Key=s3_image_filename, Body=req_data)
+        req = requests.get(url, stream=True)
+
+        content_type = req.headers["content-type"]
+        if content_type not in viable_content.keys():
+            print(f"Cannot process {url} with type {content_type}")
+            pass
+        filename += viable_content[content_type]
+
+        req_raw = req.raw
+        req_data = req_raw.read()
+        s3.Bucket(bucket).put_object(
+            Key=filename, Body=req_data, ContentType=content_type
+        )
+        print(f"success, uploaded {filename} to {bucket}")
+
+
+if __name__ == "__main__":
+    urls = [
+        "https://i.redd.it/47fibpjxk8e81.gif",
+        "https://i.redd.it/j4hlk7x8e1f81.jpg",
+    ]
+    filenames = ["3", "4"]
+    bucket = "analog-photos"
+
+    s3_upload(bucket, urls, filenames)
