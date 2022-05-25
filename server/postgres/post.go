c37fef1d6dd0fa7834f3d7cb70a7b027990c19f4
evan slack <evan.slack@tufts.edu>
2022-05-24 23:36:50 -0400

    implement find and delete post methods

--- server/postgres/post.go
+++ server/postgres/post.go
@@ -11,7 +11,8 @@ import (
 	"github.com/evanofslack/analogdb"
 )
 
-// var _ analogdb.PostService = (*PostService)(nil)
+// ensure interface is implemented
+var _ analogdb.PostService = (*PostService)(nil)
 
 // rawPost corresponds to the columns as a post is stored in the DB
 type rawPost struct {
@@ -53,7 +54,7 @@ const (
 	random = "random"
 )
 
-func (s *PostService) LatestPosts(ctx context.Context, filter analogdb.PostFilter) ([]*analogdb.Post, int, error) {
+func (s *PostService) LatestPosts(ctx context.Context, filter *analogdb.PostFilter) ([]*analogdb.Post, int, error) {
 	tx, err := s.db.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil, 0, err
@@ -62,7 +63,7 @@ func (s *PostService) LatestPosts(ctx context.Context, filter analogdb.PostFilte
 	return findPosts(ctx, tx, filter, time)
 }
 
-func (s *PostService) TopPosts(ctx context.Context, filter analogdb.PostFilter) ([]*analogdb.Post, int, error) {
+func (s *PostService) TopPosts(ctx context.Context, filter *analogdb.PostFilter) ([]*analogdb.Post, int, error) {
 	tx, err := s.db.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil, 0, err
@@ -71,7 +72,7 @@ func (s *PostService) TopPosts(ctx context.Context, filter analogdb.PostFilter) 
 	return findPosts(ctx, tx, filter, score)
 }
 
-func (s *PostService) RandomPosts(ctx context.Context, filter analogdb.PostFilter) ([]*analogdb.Post, int, error) {
+func (s *PostService) RandomPosts(ctx context.Context, filter *analogdb.PostFilter) ([]*analogdb.Post, int, error) {
 	tx, err := s.db.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil, 0, err
@@ -80,7 +81,39 @@ func (s *PostService) RandomPosts(ctx context.Context, filter analogdb.PostFilte
 	return findPosts(ctx, tx, filter, random)
 }
 
-func findPosts(ctx context.Context, tx *sql.Tx, filter analogdb.PostFilter, sort string) ([]*analogdb.Post, int, error) {
+func (s *PostService) FindPostByID(ctx context.Context, id int) (*analogdb.Post, error) {
+	tx, err := s.db.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+	posts, _, err := findPosts(ctx, tx, &analogdb.PostFilter{ID: &id}, time)
+	if err != nil {
+		return nil, err
+	} else if len(posts) == 0 {
+		return nil, errors.New("post not found")
+	}
+	return posts[0], nil
+}
+
+func (s *PostService) DeletePost(ctx context.Context, id int) (*analogdb.Post, error) {
+	tx, err := s.db.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+	posts, err := deletePost(ctx, tx, id)
+	if err != nil {
+		return nil, err
+	}
+	if len(posts) == 0 {
+		return nil, errors.New("post not found")
+	}
+	return posts[0], nil
+}
+
+// findPosts is the general function responsible for handling all queries
+func findPosts(ctx context.Context, tx *sql.Tx, filter *analogdb.PostFilter, sort string) ([]*analogdb.Post, int, error) {
 	if err := validateSort(sort); err != nil {
 		return nil, 0, err
 	}
@@ -165,8 +198,57 @@ func findPosts(ctx context.Context, tx *sql.Tx, filter analogdb.PostFilter, sort
 	return posts, count, nil
 }
 
+func deletePost(ctx context.Context, tx *sql.Tx, id int) ([]*analogdb.Post, error) {
+	query := "DELETE FROM pictures WHERE id = $1"
+
+	rows, err := tx.QueryContext(ctx, query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	posts := make([]*analogdb.Post, 0)
+	for rows.Next() {
+		var p rawPost
+		if err := rows.Scan(
+			&p.id,
+			&p.url,
+			&p.title,
+			&p.author,
+			&p.permalink,
+			&p.score,
+			&p.nsfw,
+			&p.grayscale,
+			&p.time,
+			&p.width,
+			&p.height,
+			&p.sprocket,
+			&p.lowUrl,
+			&p.lowWidth,
+			&p.lowHeight,
+			&p.medUrl,
+			&p.medWidth,
+			&p.medHeight,
+			&p.highUrl,
+			&p.highWidth,
+			&p.highHeight,
+		); err != nil {
+			return nil, err
+		}
+		lowImage := analogdb.Image{Label: "low", Url: p.lowUrl, Width: p.lowWidth, Height: p.lowHeight}
+		medImage := analogdb.Image{Label: "medium", Url: p.medUrl, Width: p.medWidth, Height: p.medHeight}
+		highImage := analogdb.Image{Label: "high", Url: p.highUrl, Width: p.highWidth, Height: p.highHeight}
+		rawImage := analogdb.Image{Label: "raw", Url: p.url, Width: p.width, Height: p.height}
+		images := []analogdb.Image{lowImage, medImage, highImage, rawImage}
+
+		post := &analogdb.Post{Id: p.id, Images: images, Title: p.title, Author: p.author, Permalink: p.permalink, Score: p.score, Nsfw: p.nsfw, Grayscale: p.grayscale, Time: p.time, Sprocket: p.sprocket}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
 // sortToOrder converts sort string into an SQL ORDER BY statement
-func sortToOrder(sort string, filter analogdb.PostFilter) string {
+func sortToOrder(sort string, filter *analogdb.PostFilter) string {
 	if sort == time {
 		return " ORDER BY time DESC"
 	} else if sort == score {
@@ -174,12 +256,14 @@ func sortToOrder(sort string, filter analogdb.PostFilter) string {
 	} else if seed := filter.Seed; seed != nil {
 		return fmt.Sprintf(" ORDER BY MOD(time, %d), time DESC", seed)
 	} else {
-		return fmt.Sprintf(" ORDER BY MOD(time, %d), time DESC", seedGenerator())
+		newSeed := seedGenerator()
+		filter.Seed = &newSeed
+		return fmt.Sprintf(" ORDER BY MOD(time, %d), time DESC", newSeed)
 	}
 }
 
 // formatLimit turns the limit into an SQL limit statement
-func formatLimit(filter analogdb.PostFilter) string {
+func formatLimit(filter *analogdb.PostFilter) string {
 	if limit := filter.Limit; limit != nil {
 		if *limit > 0 {
 			return fmt.Sprintf(` LIMIT %d`, *limit)
@@ -196,7 +280,7 @@ func seedGenerator() int {
 }
 
 // filterToWhere converts a PostFilter to an SQL WHERE statement
-func filterToWhere(filter analogdb.PostFilter) (string, []any) {
+func filterToWhere(filter *analogdb.PostFilter) (string, []any) {
 	index := 1
 	where, args := []string{"1=1"}, []any{}
 	if time := filter.Time; time != nil {
@@ -232,12 +316,35 @@ func filterToWhere(filter analogdb.PostFilter) (string, []any) {
 		args = append(args, *sprocket)
 		index += 1
 	}
+	if id := filter.ID; id != nil {
+		where = append(where, fmt.Sprintf("id = $%d", index))
+		args = append(args, *id)
+		index += 1
+	}
+	// match partial text in post title with ILIKE
+	if title := filter.Title; title != nil {
+		where = append(where, fmt.Sprintf("title ILIKE $%d", index))
+		args = append(args, "%"+*title+"%")
+		index += 1
+	}
+	// if query does not prefix author with 'u/' we need to add it
+	if author := filter.Author; author != nil {
+		var matchAuthor string
+		if pre := (*author)[0:2]; pre != "u/" {
+			matchAuthor = "u/" + *author
+		} else {
+			matchAuthor = *author
+		}
+		where = append(where, fmt.Sprintf("author = $%d", index))
+		args = append(args, matchAuthor)
+		index += 1
+	}
 	return `WHERE ` + strings.Join(where, " AND "), args
 
 }
 
 // validateFilter ensures that provided filter parameters work with sort method
-func validateFilter(sort string, filter analogdb.PostFilter) error {
+func validateFilter(sort string, filter *analogdb.PostFilter) error {
 	if sort == time {
 		if filter.Score != nil || filter.Seed != nil {
 			return errors.New("Can not include score or seed in filter if sorting by time")
--- server/postgres/post_test.go
+++ server/postgres/post_test.go
@@ -8,130 +8,254 @@ import (
 	"github.com/evanofslack/analogdb"
 )
 
-// number of posts matching each criteria in test DB
 const (
+	// number of posts matching each query from test DB
 	totalPosts     = 51
 	totalNsfw      = 4
 	totalGrayscale = 7
 	totalSprocket  = 2
+	totalPortra    = 17
+)
+
+var (
+	// reusable general filters
+	limit       = 20
+	limitFilter = &analogdb.PostFilter{Limit: &limit}
+	nilFilter   = &analogdb.PostFilter{}
+
+	// sample post
+	postID     = 2066
+	postTitle  = "Up on Melancholy Hill [Canon TLB / 28-55mm 3.5 / Portra 400]"
+	postAuthor = "u/sunnyintheoffice"
 )
 
 func TestFindPosts(t *testing.T) {
 	t.Run("Default", func(t *testing.T) {
 		ctx, tx := setupTx(t)
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, nilFilter, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalPosts; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of postsG %v, want %v", got, want)
 		} else if got, want := count, totalPosts; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("Limit", func(t *testing.T) {
 		ctx, tx := setupTx(t)
-		limit := 20
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Limit: &limit}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, limitFilter, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), limit; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalPosts; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("NoNSFW", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		nsfw := false
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Nsfw: &nsfw}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Nsfw: &nsfw}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalPosts-totalNsfw; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalPosts-totalNsfw; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("OnlyNSFW", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		nsfw := true
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Nsfw: &nsfw}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Nsfw: &nsfw}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalNsfw; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalNsfw; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("NoBW", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		grayscale := false
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Grayscale: &grayscale}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Grayscale: &grayscale}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalPosts-totalGrayscale; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalPosts-totalGrayscale; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("OnlyBW", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		grayscale := true
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Grayscale: &grayscale}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Grayscale: &grayscale}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalGrayscale; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalGrayscale; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("NoSprocket", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		sprocket := false
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Sprocket: &sprocket}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Sprocket: &sprocket}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalPosts-totalSprocket; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalPosts-totalSprocket; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
 	t.Run("OnlySprocket", func(t *testing.T) {
 		ctx, tx := setupTx(t)
 		sprocket := true
-		if posts, count, err := findPosts(ctx, tx, analogdb.PostFilter{Sprocket: &sprocket}, "time"); err != nil {
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Sprocket: &sprocket}, "time"); err != nil {
 			t.Fatal(err)
 		} else if got, want := len(posts), totalSprocket; got != want {
-			t.Fatalf("length of posts = %v, want %v", got, want)
+			t.Fatalf("length of posts %v, want %v", got, want)
 		} else if got, want := count, totalSprocket; got != want {
-			t.Fatalf("total count = %v, want %v", got, want)
+			t.Fatalf("total count %v, want %v", got, want)
 		}
 	})
 
+	t.Run("ByAuthor", func(t *testing.T) {
+		ctx, tx := setupTx(t)
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Author: &postAuthor}, "time"); err != nil {
+			t.Fatal(err)
+		} else if len(posts) != 1 || count != 1 {
+			t.Fatal("must be one matching post")
+		} else if got, want := posts[0].Title, postTitle; got != want {
+			t.Fatalf("Post title does not match, got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("ByAuthorAddPrefix", func(t *testing.T) {
+		ctx, tx := setupTx(t)
+		noPrefixAuthor := postAuthor[2:]
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Author: &noPrefixAuthor}, "time"); err != nil {
+			t.Fatal(err)
+		} else if len(posts) != 1 || count != 1 {
+			t.Fatal("must be one matching post")
+		} else if got, want := posts[0].Title, postTitle; got != want {
+			t.Fatalf("Post title does not match, got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("SearchTitleOne", func(t *testing.T) {
+		ctx, tx := setupTx(t)
+		keyword := "Melancholy"
+		if posts, count, err := findPosts(ctx, tx, &analogdb.PostFilter{Title: &keyword}, "time"); err != nil {
+			t.Fatal(err)
+		} else if len(posts) != 1 || count != 1 {
+			t.Fatal("must be one matching post")
+		} else if got, want := posts[0].Title, postTitle; got != want {
+			t.Fatalf("Post title does not match, got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("SearchTitleMultiple", func(t *testing.T) {
+		ctx, tx := setupTx(t)
+		keyword := "Portra"
+		if posts, _, err := findPosts(ctx, tx, &analogdb.PostFilter{Title: &keyword}, "time"); err != nil {
+			t.Fatal(err)
+		} else if got, want := len(posts), totalPortra; got != want {
+			t.Fatalf("number of matching titles not equal, got %v, want %v", got, want)
+		}
+	})
 }
 
 func TestLatestPost(t *testing.T) {
-	db := MustOpen(t)
-	defer MustClose(t, db)
-	psql := NewPostService(db)
+	t.Run("PostSequential", func(t *testing.T) {
+		db := MustOpen(t)
+		defer MustClose(t, db)
+		ps := NewPostService(db)
 
-	limit := 20
-	filter := analogdb.PostFilter{
-		Limit: &limit,
-	}
+		posts, _, err := ps.LatestPosts(context.Background(), limitFilter)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if posts, count, err := psql.LatestPosts(context.Background(), filter); err != nil {
-		t.Fatal(err)
-	} else if got, want := len(posts), limit; got != want {
-		t.Fatalf("length of posts = %v, want %v", got, want)
-	} else if got, want := count, 51; got != want {
-		t.Fatalf("total count = %v, want %v", got, want)
-	}
+		newest := posts[0].Time
+		for _, p := range posts {
+			if p.Time > newest {
+				t.Fatalf("posts not sorted newest to oldest")
+			}
+		}
+	})
+}
 
+func TestTopPost(t *testing.T) {
+	t.Run("PostTop", func(t *testing.T) {
+		db := MustOpen(t)
+		defer MustClose(t, db)
+		ps := NewPostService(db)
+
+		posts, _, err := ps.TopPosts(context.Background(), limitFilter)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		top := posts[0].Score
+		for _, p := range posts {
+			if p.Score > top {
+				t.Fatalf("posts not sorted most to least votes")
+			}
+		}
+	})
+}
+
+func TestRandomPost(t *testing.T) {
+	t.Run("UpdateFilterSeed", func(t *testing.T) {
+		db := MustOpen(t)
+		defer MustClose(t, db)
+		ps := NewPostService(db)
+
+		tempFilter := &analogdb.PostFilter{Limit: &limit}
+		if seed := tempFilter.Seed; seed != nil {
+			t.Fatal("unset seed must be nil")
+		}
+
+		_, _, err := ps.RandomPosts(context.Background(), tempFilter)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if seed := tempFilter.Seed; seed == nil {
+			t.Fatal("assigned seed must not be nil")
+		}
+	})
+}
+
+func TestFindPost(t *testing.T) {
+	t.Run("ErrNoPost", func(t *testing.T) {
+		db := MustOpen(t)
+		defer MustClose(t, db)
+		ps := NewPostService(db)
+
+		if _, err := ps.FindPostByID(context.Background(), 69); err == nil {
+			t.Fatal("error should be returned when no matching post is found")
+		}
+	})
+
+	t.Run("ByID", func(t *testing.T) {
+		db := MustOpen(t)
+		defer MustClose(t, db)
+		ps := NewPostService(db)
+
+		post, err := ps.FindPostByID(context.Background(), postID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got, want := post.Title, postTitle; got != want {
+			t.Fatalf("Post title does not match, got %v, want %v", got, want)
+		}
+	})
 }
 
 func setupTx(t *testing.T) (context.Context, *sql.Tx) {
@@ -143,6 +267,5 @@ func setupTx(t *testing.T) (context.Context, *sql.Tx) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	return ctx, tx
 }
