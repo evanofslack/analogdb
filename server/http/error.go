a4a7e7958a7f6105cc8ed8c4289b43c7d1288ab3
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-05-25 20:06:42 -0400

    add error handling and start post handler

---
+++ server/http/error.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+const internalError = "Internal error."
+
+func writeError(w http.ResponseWriter, r *http.Request, err error) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(&ErrorResponse{Error: internalError})
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
---
+++ server/http/posts.go
@@ -0,0 +1,46 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/evanofslack/analogdb"
+)
+
+type Meta struct {
+	TotalPosts int    `json:"total_posts"`
+	PageSize   int    `json:"page_size"`
+	PageID     string `json:"next_page_id"`
+	PageURL    string `json:"next_page_url"`
+	Seed       int    `json:"seed,omitempty"`
+}
+
+type Response struct {
+	Meta  Meta            `json:"meta"`
+	Posts []analogdb.Post `json:"posts"`
+}
+
+func (s *Server) latestPosts(w http.ResponseWriter, r *http.Request) {
+	filter := &analogdb.PostFilter{}
+	posts, count, err := s.PostService.LatestPosts(r.Context(), filter)
+	if err != nil {
+		writeError(w, r, err)
+	}
+
+}
+
+func parseToFilter(r *http.Request) *analogdb.PostFilter {
+	limit := r.URL.Query().Get("page_size")
+	pageID := r.URL.Query().Get("page_id")
+	nsfw := r.URL.Query().Get("nsfw")
+	grayscale := r.URL.Query().Get("bw")
+	sprocket := r.URL.Query().Get("sprocket")
+	seed := r.URL.Query().Get("seed")
+	id := r.URL.Query().Get("id")
+	title := r.URL.Query().Get("title")
+	author := r.URL.Query().Get("author")
+
+}
+
+func queryParam(r *http.Request, key string) string {
+	return r.URL.Query().Get(key)
+}
