3b324a29207dbda1b8562bc7eddd02c99f7a931d
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-05-27 12:53:04 -0400

    remove old implementation code

--- server/cmd/analogdb/main.go
+++ server/cmd/analogdb/main.go
@@ -3,67 +3,38 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	mw "github.com/evanofslack/analogdb/middleware"
-	"github.com/evanofslack/analogdb/models"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	"github.com/evanofslack/analogdb/http"
+	"github.com/evanofslack/analogdb/postgres"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DBUSER"),
+		os.Getenv("DBPASSWORD"), os.Getenv("DBNAME"))
+
+	db := postgres.NewDB(dsn)
+	if err := db.Open(); err != nil {
 		log.Fatal(err)
 	}
 
-	err = models.InitDB(false)
-	if err != nil {
-		log.Fatal(err)
-	}
+	ps := postgres.NewPostService(db)
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "DELETE"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           500,
-	}))
+	s := http.NewServer()
+	s.PostService = ps
+	s.MountMiddleware()
+	s.MountStatic()
+	s.MountStatus()
+	s.MountPostHandlers()
+	s.Run()
 
-	r.Handle("/*", http.FileServer(http.Dir("./static")))
-
-	r.Group(func(r chi.Router) {
-		r.Use(mw.Pagination)
-		r.Get("/latest", listLatest)
-		r.Get("/top", listTop)
-		r.Get("/random", listRandom)
-		r.Get("/nsfw", listNsfw)
-		r.Get("/bw", listBw)
-		r.Get("/sprocket", listSprocket)
-	})
-
-	r.Route("/posts/{id}", func(r chi.Router) {
-		r.Get("/", findPost)
-		r.With(mw.BasicAuth).Delete("/", deletePost)
-	})
-
-	fmt.Println("listening...")
-	http.ListenAndServe(getPort(), r)
-}
-
-func getPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "5000"
-		fmt.Println("No PORT env variable found, defaulting to: " + port)
-	}
-	return ":" + port
 }
