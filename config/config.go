5f9d10418cdd8e5221b8e7abd7efed2797b547b4
evan slack <evan.slack@tufts.edu>
2022-06-05 17:30:59 -0400

    add config options

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	App  `yaml:"app"`
+	DB   `yaml:"postgres"`
+	HTTP `yaml:"http"`
+	Log  `yaml:"logger"`
+}
+
+type App struct {
+	Name    string `yaml:"name" env:"APP_NAME"`
+	Version string `yaml:"version" env:"APP_VERSION"`
+}
+
+type DB struct {
+	URL string `yaml:"url" env:"DATABASE_URL"`
+}
+
+type HTTP struct {
+	Port string `yaml:"port" env:"HTTP_PORT"`
+}
+
+type Log struct {
+	Level string `yaml:"level" env:"LOG_LEVEL"`
+}
+
+func New() (*Config, error) {
+	cfg := &Config{}
+
+	if err := cleanenv.ReadConfig("./config/config.yml", cfg); err != nil {
+		return nil, fmt.Errorf("Error loading config: %w", err)
+	}
+	if err := cleanenv.ReadEnv(cfg); err != nil {
+		return nil, fmt.Errorf("Error loading env: %w", err)
+	}
+	return cfg, nil
+}
