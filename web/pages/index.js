5a1a3762e8c603c85a396b3dda9dac9e763edc5d
Evan Slack <51209817+evanofslack@users.noreply.github.com>
2022-07-19 21:01:23 -0400

    Modify landing page backend query
    Increase landing page number of photos and hide NSFW

--- web/pages/index.js
+++ web/pages/index.js
@@ -3,7 +3,7 @@ import Template from "../components/template";
 const baseURL = "https://analogdb.herokuapp.com";
 
 export async function getServerSideProps(context) {
-    const url = baseURL + "/latest?page_size=25&bw=false";
+    const url = baseURL + "/latest?page_size=50&bw=false&nsfw=false";
     const response = await fetch(url);
     const data = await response.json();
     return {
