94b399324a89cfe2880fa086b3b5e82c21c51dc3
evanofslack <evan.slack@outlook.com>
2022-11-30 23:30:00 -0500

    add constants file

---
+++ constants.py
@@ -0,0 +1,22 @@
+CLOUDFRONT_URL = "https://d3i73ktnzbi69i.cloudfront.net/"
+AWS_BUCKET = "analog-photos"
+AWS_BUCKET_TEST = "analog-photos-test"
+
+# Define subreddit names
+ANALOG_SUB = "analog"
+BW_SUB = "analog_bw"
+SPROCKET_SUB = "SprocketShots"
+
+# define number of posts to scrape per subreddit
+ANALOG_POSTS = 7
+BW_POSTS = 1
+SPROCKET_POSTS = 1
+
+# define resolutions for resizing images
+LOW_RES = (720, 720)
+MEDIUM_RES = (1080, 1080)
+HIGH_RES = (1440, 1440)
+RAW_RES = None
+
+# analogdb api url
+ANALOGDB_URL = "https://api.analogdb.com"
