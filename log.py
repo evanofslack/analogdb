f7dea77a0364c56b73e80e1b24e0719d87ac4464
evanofslack <evan.slack@outlook.com>
2022-12-04 16:14:12 -0500

    remove default logger

--- log.py
+++ log.py
@@ -7,6 +7,7 @@ def init_logger():
     format = "{time} {level} {message}"
     format_color = "<green>{time}</green> <level> {level} {message}</level>"
 
+    logger.remove(0)  # remove default handler
     logger.add(sys.stderr, colorize=True, format=format_color, level="INFO")
     logger.add("info.log", format=format, retention="1 week", level="INFO")
     logger.add("error.log", format=format, retention="2 months", level="WARNING")
