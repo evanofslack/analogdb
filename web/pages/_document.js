72972c7fdb709594a061a38f01b6e6681d4d305e
evanofslack <evan.slack@outlook.com>
2023-01-05 01:42:52 -0500

    add document with umami details

---
+++ web/pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="609a40fc-c71b-477e-a381-2c4792723925"
+          src="https://umami.eslack.net/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
