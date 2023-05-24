2256c3fa732e7fc4f29541bcd8b050057d8c90da
evan slack <evan.slack@outlook.com>
2023-05-23 22:59:51 -0400

    change nextjs image settings

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "i.redd.it",
-      "preview.redd.it",
-      "i.imgur.com",
-      "imgur.com",
-      "live.staticflickr.com",
-      "d3i73ktnzbi69i.cloudfront.net",
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d3i73ktnzbi69i.cloudfront.net",
+      },
     ],
   },
   experimental: {
