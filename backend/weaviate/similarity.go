6b317e7ef6f1d8f1aeab81a6f74590b17117f23c
evan slack <evan.slack@outlook.com>
2023-07-14 17:39:50 -0400

    delete post from vector db

--- backend/weaviate/similarity.go
+++ backend/weaviate/similarity.go
@@ -3,12 +3,16 @@ package weaviate
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/evanofslack/analogdb"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate/data/replication"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/filters"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
 	"github.com/weaviate/weaviate/entities/models"
 )
+
+const PictureClass = "Picture"
 
 var _ analogdb.SimilarityService = (*SimilarityService)(nil)
 
@@ -19,6 +23,10 @@ type SimilarityService struct {
 
 func NewSimilarityService(db *DB, ps analogdb.PostService) *SimilarityService {
 	return &SimilarityService{db: db, postService: ps}
+}
+
+func (ss SimilarityService) DeletePost(ctx context.Context, postID int) (error) {
+	return ss.db.deletePost(ctx, postID)
 }
 
 func (ss SimilarityService) FindSimilarPostsByImage(ctx context.Context, postID int, similarityFilter *analogdb.PostSimilarityFilter) ([]*analogdb.Post, error) {
@@ -41,6 +49,50 @@ func (ss SimilarityService) FindSimilarPostsByImage(ctx context.Context, postID 
 	return posts, err
 }
 
+
+func (db *DB) deletePost(ctx context.Context, postID int) (error) {
+
+	db.logger.Debug().Int("postID", postID).Msg("Starting delete post from vector DB")
+	 
+	where := filters.Where().
+		WithPath([]string{"post_id"}).
+		WithOperator(filters.Equal).
+		WithValueInt(int64(postID))
+
+	result, err := db.db.GraphQL().Get().
+		WithClassName(PictureClass).
+		WithLimit(1).
+		WithWhere(where).
+		Do(ctx)
+
+	if err != nil {
+		err = fmt.Errorf("Failed to find postID in vector DB, err=%w", err)
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to delete post from vectorDB")
+		return &analogdb.Error{Code: analogdb.ERRNOTFOUND, Message: fmt.Sprintf("Post %d not found", postID)}
+	}
+
+	pics, err := unmarshallPicturesResp(result)
+	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to delete post from vector DB")
+		return &analogdb.Error{Code: analogdb.ERRNOTFOUND, Message: fmt.Sprintf("Post %d not found", postID)}
+	}
+
+	err = db.db.Data().Deleter().
+		WithClassName(PictureClass).
+		WithID(pics[0].uuid).
+		WithConsistencyLevel(replication.ConsistencyLevel.ALL).  // default QUORUM
+		Do(ctx)
+
+  if err != nil {
+	db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to delete post from vector DB")
+	return &analogdb.Error{Code: analogdb.ERRINTERNAL, Message: fmt.Sprintf("Post %d could not be deleted from vector DB", postID)}
+  }
+
+	db.logger.Info().Int("postID", postID).Msg("Deleted post from vector DB")
+
+	return err
+}
+
 type pictureResponse struct {
 	postID   int
 	distance float64
@@ -48,6 +100,8 @@ type pictureResponse struct {
 }
 
 func (db *DB) getSimilarPostIDs(ctx context.Context, postID int, filter *analogdb.PostSimilarityFilter) ([]int, error) {
+
+	db.logger.Debug().Int("postID", postID).Msg("Starting get similar posts from vector DB")
 
 	var ids []int
 
@@ -74,12 +128,14 @@ func (db *DB) getSimilarPostIDs(ctx context.Context, postID int, filter *analogd
 		Do(ctx)
 
 	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to find post in vector DB")
 		return ids, err
 	}
 
 	pics, err := unmarshallPicturesResp(result)
-	if len(pics) == 0 {
-		return ids, &analogdb.Error{Code: analogdb.ERRNOTFOUND, Message: "Post not found"}
+	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to unmarshall post from vector DB")
+		return ids, &analogdb.Error{Code: analogdb.ERRNOTFOUND, Message: fmt.Sprintf("Post %d not found", postID)}
 	}
 
 	// then make query to find nearest neighbors
@@ -87,6 +143,7 @@ func (db *DB) getSimilarPostIDs(ctx context.Context, postID int, filter *analogd
 	// this is where we narrow down the results
 	where, err = filterToWhere(filter)
 	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to convert similarity filter to where clause")
 		return ids, err
 	}
 
@@ -106,11 +163,13 @@ func (db *DB) getSimilarPostIDs(ctx context.Context, postID int, filter *analogd
 		Do(ctx)
 
 	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to find near posts in vector DB")
 		return ids, err
 	}
 
 	pics, err = unmarshallPicturesResp(result)
 	if err != nil {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Failed to unmarshall post from vector DB")
 		return ids, err
 	}
 
@@ -119,6 +178,7 @@ func (db *DB) getSimilarPostIDs(ctx context.Context, postID int, filter *analogd
 	}
 
 	if len(ids) == 0 {
+		db.logger.Error().Err(err).Int("postID", postID).Msg("Found zero similar posts")
 		return ids, &analogdb.Error{Code: analogdb.ERRNOTFOUND, Message: "No similar posts found"}
 	}
 
@@ -202,5 +262,7 @@ func unmarshallPicturesResp(result *models.GraphQLResponse) ([]pictureResponse, 
 	if len(picturesResponse) > 0 {
 		return picturesResponse, nil
 	}
-	return picturesResponse, errors.New("Failed to unmarshall pictures from vector DB")
+
+	err := errors.New("Failed to unmarshall pictures from vector DB")
+	return picturesResponse, err
 }
