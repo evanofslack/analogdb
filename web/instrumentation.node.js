8f9acb1bad5d3efc17ba064a8f343ca186c40751
evan slack <evan.slack@outlook.com>
2023-07-29 18:39:42 -0400

    propogate nextjs server fetch contexts

--- web/instrumentation.node.js
+++ web/instrumentation.node.js
@@ -1,21 +1,53 @@
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import { Resource } from "@opentelemetry/resources";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import {
+  CompositePropagator,
+  W3CTraceContextPropagator,
+} from "@opentelemetry/core";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import {
+  detectResourcesSync,
+  envDetector,
+  Resource,
+} from "@opentelemetry/resources";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { otelURL, serviceName } from "./constants.js";
 
-const exporterOptions = {
-  url: otelURL,
-};
+export function register() {
+  registerInstrumentations({
+    instrumentations: [new HttpInstrumentation()],
+  });
 
-const traceExporter = new OTLPTraceExporter(exporterOptions);
+  const resource = Resource.default()
+    .merge(
+      new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+      })
+    )
+    .merge(detectResourcesSync({ detectors: [envDetector] }));
+  const provider = new NodeTracerProvider({ resource });
 
-const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-  }),
-  spanProcessor: new SimpleSpanProcessor(traceExporter),
-});
+  const exporter = new OTLPTraceExporter({
+    url: otelURL,
+  });
 
-sdk.start();
+  console.log("exporting OTEL traces to endpoint: " + otelURL);
+
+  let processor = new BatchSpanProcessor(exporter);
+
+  const propagator = new CompositePropagator({
+    propagators: [new W3CTraceContextPropagator()],
+  });
+
+  provider.addSpanProcessor(processor);
+
+  provider.register({ propagator });
+
+  process.on("SIGTERM", () => {
+    provider.shutdown();
+  });
+}
+
+register();
