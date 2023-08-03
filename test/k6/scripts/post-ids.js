24afe9ae22a8560a43d504420e0eb21d0a507d69
evan slack <evan.slack@outlook.com>
2023-08-03 00:01:34 -0400

    update k6 tests

--- test/k6/scripts/post-ids.js
+++ test/k6/scripts/post-ids.js
@@ -1,16 +1,42 @@
+import encoding from "k6/encoding";
 import http from "k6/http";
-import { check } from "k6";
+import { check, sleep } from "k6";
+
+const baseURL = __ENV.BASE_URL;
+const username = __ENV.USERNAME;
+const password = __ENV.PASSWORD;
+const delay = __ENV.DELAY;
+const duration = __ENV.DURATION;
+const vus = __ENV.VUS;
+
+const credentials = `${username}:${password}`;
+const encodedCredentials = encoding.b64encode(credentials);
+
+const httpOpts = {
+  headers: {
+    Authorization: `Basic ${encodedCredentials}`,
+  },
+};
+
+function get(path) {
+  let url = `${baseURL}${path}`;
+
+  // use basic auth if configured
+  if (username != "" && password != "") {
+    return http.get(`${baseURL}${path}`, httpOpts);
+  } else {
+    return http.get(`${baseURL}${path}`);
+  }
+}
+
 export const options = {
-  vus: 1,
-  duration: "3600s",
+  vus: vus,
+  duration: duration,
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
     http_req_duration: ["p(95)<100"], // 95% of requests should be below 200ms
   },
 };
-
-const baseURL = "http://analogdb:8080";
-// const baseURL = "https://api.analogdb.com";
 
 export function setup() {
   const postIDsURL = `${baseURL}/ids`;
@@ -29,8 +55,12 @@ export default function (data) {
 
   let id = ids[Math.floor(Math.random() * ids.length)];
 
-  check(http.get(`${baseURL}/post/${id}`), {
+  check(get(`/post/${id}`), {
     "status is 200": (r) => r.status == 200,
-    "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
   });
+
+  // sleep if configured
+  if (delay != 0) {
+    sleep(delay);
+  }
 }
