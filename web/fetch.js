1d54be872d5b16bdd2ba5ca2e91223631b55b5b9
Evan Slack <evan.slack@outlook.com>
2025-05-25 22:57:43 -0400

    update native fetch

--- web/fetch.js
+++ web/fetch.js
@@ -1,34 +1,22 @@
 import { baseURL } from "./constants.js";
-import getConfig from "next/config";
-import fetch from 'node-fetch';
 
-const { serverConfig } = getConfig();
-
-// for buildtime
-let username = process.env.AUTH_USERNAME;
-let password = process.env.AUTH_PASSWORD;
-
-// for runtime
-if (username == "") {
-    username = serverConfig.AUTH_USERNAME;
-}
-if (password == "") {
-    password = serverConfig.AUTH_PASSWORD;
-}
-
-
-export async function authorized_fetch(route, method) {
-
+export async function authorized_fetch(route, method = "GET") {
     const url = `${baseURL}${route}`;
-    let headers = new Headers();
+    let headers = {};
 
-    if (username != "" && password != "") {
-        let auth =
-            "Basic " + Buffer.from(username + ":" + password).toString("base64");
-        headers.append("Authorization", auth);
+    const username = process.env.AUTH_USERNAME;
+    const password = process.env.AUTH_PASSWORD;
+
+    if (username && password) {
+        const auth = Buffer.from(`${username}:${password}`).toString("base64");
+        headers["Authorization"] = `Basic ${auth}`;
     }
 
-    const response = await fetch(url, { method: method, headers: headers });
+    const response = await fetch(url, {
+        method: method,
+        headers: headers,
+        next: { revalidate: 60 }
+    });
 
     return response;
 }
