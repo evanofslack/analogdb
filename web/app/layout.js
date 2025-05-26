34475a57b3610945387249272adac41cfa0a4ac9
Evan Slack <evan.slack@outlook.com>
2025-05-26 16:01:30 -0400

    format

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,19 +1,22 @@
 import { ClientProviders } from './providers/client-providers'
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import '../styles/globals.css'
 
 export const metadata = {
-  title: 'AnalogDB',
-  description: 'Film photography community archive',
+    title: 'AnalogDB',
+    description: 'Film photography community archive',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <ClientProviders>
-          {children}
-        </ClientProviders>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body>
+                <NuqsAdapter>
+                    <ClientProviders>
+                        {children}
+                    </ClientProviders>
+                </NuqsAdapter>
+            </body>
+        </html>
+    )
 }
