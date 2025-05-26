c5d740341fac18bfd230ab0b9bc9379e0709fa54
Evan Slack <evan.slack@outlook.com>
2025-05-26 17:49:38 -0400

    remove client providers

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,21 +1,43 @@
-import { ClientProviders } from './providers/client-providers'
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import '../styles/globals.css'
+import '@mantine/core/styles.css';
+import '@mantine/code-highlight/styles.css';
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { BreakpointProvider } from "../providers/breakpoint";
+import { NavigationProgress } from "@mantine/nprogress";
+import { CodeHighlightProvider } from '../components/CodeHighlightProvider';
 
 export const metadata = {
     title: 'AnalogDB',
-    description: 'Film photography community archive',
+    description: 'The collection of film photography',
 }
+
+const queries = {
+    xs: "(max-width: 480px)",
+    sm: "(max-width: 720px)",
+    md: "(max-width: 1024px)",
+    lg: "(max-width: 1440px)",
+    xl: "(max-width: 2048px)",
+};
+
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" {...mantineHtmlProps} >
+            <head>
+                <ColorSchemeScript />
+            </head>
             <body>
-                <NuqsAdapter>
-                    <ClientProviders>
-                        {children}
-                    </ClientProviders>
-                </NuqsAdapter>
+                <MantineProvider>
+                    <CodeHighlightProvider>
+                        <NuqsAdapter>
+                            <NavigationProgress />
+                            <BreakpointProvider queries={queries}>
+                                {children}
+                            </BreakpointProvider>
+                        </NuqsAdapter>
+                    </CodeHighlightProvider>
+                </MantineProvider>
             </body>
         </html>
     )
