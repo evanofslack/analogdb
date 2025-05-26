90b2526108c903354403e389bd3a0c75719190c6
Evan Slack <evan.slack@outlook.com>
2025-05-26 00:16:49 -0400

    provider for client

--- web/app/home-page.js
+++ web/app/home-page.js
@@ -1,7 +1,20 @@
 'use client';
 
 import Gallery from "../components/gallery";
+import { BreakpointProvider } from "../providers/breakpoint";
+
+const queries = {
+  xs: "(max-width: 480px)",
+  sm: "(max-width: 720px)",
+  md: "(max-width: 1024px)",
+  lg: "(max-width: 1440px)",
+  xl: "(max-width: 2048px)",
+};
 
 export default function HomePage({ data }) {
-  return <Gallery data={data} />;
+  return (
+    <BreakpointProvider queries={queries}>
+      <Gallery data={data} />
+    </BreakpointProvider>
+  );
 }
