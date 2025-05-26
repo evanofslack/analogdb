07ad50fb9ee2d6f223e9651d2e93463747719a48
Evan Slack <evan.slack@outlook.com>
2025-05-25 22:55:24 -0400

    format

--- web/fetch.js
+++ web/fetch.js
@@ -10,25 +10,25 @@ let password = process.env.AUTH_PASSWORD;
 
 // for runtime
 if (username == "") {
-  username = serverConfig.AUTH_USERNAME;
+    username = serverConfig.AUTH_USERNAME;
 }
 if (password == "") {
-  password = serverConfig.AUTH_PASSWORD;
+    password = serverConfig.AUTH_PASSWORD;
 }
 
 
 export async function authorized_fetch(route, method) {
 
-  const url = `${baseURL}${route}`;
-  let headers = new Headers();
+    const url = `${baseURL}${route}`;
+    let headers = new Headers();
 
-  if (username != "" && password != "") {
-    let auth =
-      "Basic " + Buffer.from(username + ":" + password).toString("base64");
-    headers.append("Authorization", auth);
-  }
+    if (username != "" && password != "") {
+        let auth =
+            "Basic " + Buffer.from(username + ":" + password).toString("base64");
+        headers.append("Authorization", auth);
+    }
 
-  const response = await fetch(url, { method: method, headers: headers });
+    const response = await fetch(url, { method: method, headers: headers });
 
-  return response;
+    return response;
 }
