37ae158af0897eab7002d3a34debdfbcb49fad81
Evan Slack <evan.slack@outlook.com>
2025-05-26 16:15:40 -0400

    port over about and docs pages

---
+++ web/app/about/page.js
@@ -0,0 +1,36 @@
+import { authorized_fetch } from "../../fetch.js";
+import About from "../../components/about";
+import Header from "../../components/header";
+import styles from "../../components/gallery.module.css";
+
+export const metadata = {
+    title: 'AnalogDB',
+    description: 'Film photography database',
+};
+
+export const revalidate = 60; // Revalidate every 60 seconds
+
+async function getData() {
+    const postsRoute = "/posts";
+    const postsResponse = await authorized_fetch(postsRoute, "GET");
+    const postsData = await postsResponse.json();
+    const numPosts = postsData.meta.total_posts;
+
+    const authorsRoute = "/authors";
+    const authorsResponse = await authorized_fetch(authorsRoute, "GET");
+    const authorsData = await authorsResponse.json();
+    const numAuthors = [...new Set(authorsData.authors)].length;
+
+    return { numPosts: numPosts, numAuthors: numAuthors };
+}
+
+export default async function AboutPage() {
+    const data = await getData();
+    
+    return (
+        <div className={styles.container}>
+            <Header />
+            <About data={data} />
+        </div>
+    );
+}
---
+++ web/app/docs/page.js
@@ -0,0 +1,17 @@
+import Documentation from "../../components/documentation";
+import Header from "../../components/header";
+import styles from "../../components/gallery.module.css";
+
+export const metadata = {
+    title: 'AnalogDB',
+    description: 'Film photography database',
+};
+
+export default function Docs() {
+    return (
+        <div className={styles.container}>
+            <Header />
+            <Documentation />
+        </div>
+    );
+}
