aaf703d9aaafaadead66f168b9a9bf7b6cef36a3
Evan Slack <evan.slack@outlook.com>
2025-05-25 23:31:34 -0400

    nav usePathname

--- web/components/mobileNav.js
+++ web/components/mobileNav.js
@@ -1,62 +1,63 @@
+'use client';
 import { AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
 import styles from "./mobileNav.module.css";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { BiCheck } from "react-icons/bi";
 import { GrClose } from "react-icons/gr";
 import { FiGithub } from "react-icons/fi";
 
 export default function MobileNav() {
-  const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen((value) => !value);
+    const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen((value) => !value);
 
-  return (
-    <div>
-      <AiOutlineMenu size="1.8rem" onClick={toggle} />
-      {isOpen && (
-        <div className={styles.blur}>
-          <div className={styles.headerContainer}>
-            <div className={styles.close}>
-              <GrClose size="1.5rem" onClick={toggle} />
-            </div>
+    return (
+        <div>
+            <AiOutlineMenu size="1.8rem" onClick={toggle} />
+            {isOpen && (
+                <div className={styles.blur}>
+                    <div className={styles.headerContainer}>
+                        <div className={styles.close}>
+                            <GrClose size="1.5rem" onClick={toggle} />
+                        </div>
 
-            <nav className={styles.navContainer}>
-              <Link href="/" className={styles.link}>
-                <div className={styles.icon}>
-                  <div className={styles.check}>
-                    <h1 className={styles.iconText}>GALLERY</h1>
-                    {router.pathname == "/" && <BiCheck size="2rem" />}
-                  </div>
+                        <nav className={styles.navContainer}>
+                            <Link href="/" className={styles.link}>
+                                <div className={styles.icon}>
+                                    <div className={styles.check}>
+                                        <h1 className={styles.iconText}>GALLERY</h1>
+                                        {pathname === "/" && <BiCheck size="2rem" />}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link href="/about" className={styles.link}>
+                                <div className={styles.icon}>
+                                    <div className={styles.check}>
+                                        <h1 className={styles.iconText}>ABOUT</h1>
+                                        {pathname === "/about" && <BiCheck size="2rem" />}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link href="/docs" className={styles.link}>
+                                <div className={styles.icon}>
+                                    <div className={styles.check}>
+                                        <h1 className={styles.iconText}>API</h1>
+                                        {pathname === "/docs" && <BiCheck size="2rem" />}
+                                    </div>
+                                </div>
+                            </Link>
+                        </nav>
+                        <div className={styles.footer}>
+                            <p> &copy; 2022 analogdb </p>
+                            <a href="https://github.com/evanofslack/analogdb">
+                                <FiGithub size="1.2rem" />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-              </Link>
-              <Link href="/about" className={styles.link}>
-                <div className={styles.icon}>
-                  <div className={styles.check}>
-                    <h1 className={styles.iconText}>ABOUT</h1>
-                    {router.pathname == "/about" && <BiCheck size="2rem" />}
-                  </div>
-                </div>
-              </Link>
-              <Link href="/docs" className={styles.link}>
-                <div className={styles.icon}>
-                  <div className={styles.check}>
-                    <h1 className={styles.iconText}>API</h1>
-                    {router.pathname == "/docs" && <BiCheck size="2rem" />}
-                  </div>
-                </div>
-              </Link>
-            </nav>
-            <div className={styles.footer}>
-              <p> &copy; 2022 analogdb </p>
-              <a href="https://github.com/evanofslack/analogdb">
-                <FiGithub size="1.2rem" />
-              </a>
-            </div>
-          </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
--- web/components/webNav.js
+++ web/components/webNav.js
@@ -1,45 +1,45 @@
 import styles from "./webNav.module.css";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import { useBreakpoint } from "../providers/breakpoint.js";
 
 export default function WebNav() {
-  const router = useRouter();
-  const breakpoints = useBreakpoint();
+    const pathname = usePathname();
+    const breakpoints = useBreakpoint();
 
-  let useMobile = false;
-  if (breakpoints["sm"]) {
-    useMobile = true;
-  }
-  if (useMobile) {
-    return null;
-  }
-  return (
-    <nav>
-      <div className={styles.headerContainer}>
-        <Link
-          href="/"
-          className={router.pathname == "/" ? styles.linkOn : styles.linkOff}
-        >
-          GALLERY
-        </Link>
-        <Link
-          href="/about"
-          className={
-            router.pathname == "/about" ? styles.linkOn : styles.linkOff
-          }
-        >
-          ABOUT
-        </Link>
-        <Link
-          href="/docs"
-          className={
-            router.pathname == "/docs" ? styles.linkOn : styles.linkOff
-          }
-        >
-          API
-        </Link>
-      </div>
-    </nav>
-  );
+    let useMobile = false;
+    if (breakpoints["sm"]) {
+        useMobile = true;
+    }
+    if (useMobile) {
+        return null;
+    }
+    return (
+        <nav>
+            <div className={styles.headerContainer}>
+                <Link
+                    href="/"
+                    className={router.pathname == "/" ? styles.linkOn : styles.linkOff}
+                >
+                    GALLERY
+                </Link>
+                <Link
+                    href="/about"
+                    className={
+                        pathname == "/about" ? styles.linkOn : styles.linkOff
+                    }
+                >
+                    ABOUT
+                </Link>
+                <Link
+                    href="/docs"
+                    className={
+                        pathname == "/docs" ? styles.linkOn : styles.linkOff
+                    }
+                >
+                    API
+                </Link>
+            </div>
+        </nav>
+    );
 }
