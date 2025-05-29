97a2f119707876a2674d94e7ded2c3131937e733
Evan Slack <evan.slack@outlook.com>
2025-05-28 21:39:04 -0400

    config

--- web/next.config.js
+++ web/next.config.js
@@ -1,28 +1,25 @@
 module.exports = {
-    output: "standalone",
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "d3i73ktnzbi69i.cloudfront.net",
-            },
-        ],
-    },
-    experimental: {
-        scrollRestoration: true,
-        optimizePackageImports: [
-            "@mantine/core",
-            "@mantine/nprogress"
-        ]
-    },
-    env: {
-        AUTH_USERNAME: process.env.AUTH_USERNAME,
-        AUTH_PASSWORD: process.env.AUTH_PASSWORD,
-    },
-    serverRuntimeConfig: {
-        AUTH_USERNAME: process.env.AUTH_USERNAME,
-        AUTH_PASSWORD: process.env.AUTH_PASSWORD,
-    },
+  output: "standalone",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d3i73ktnzbi69i.cloudfront.net",
+      },
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+    optimizePackageImports: ["@mantine/core"],
+  },
+  env: {
+    AUTH_USERNAME: process.env.AUTH_USERNAME,
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+  },
+  serverRuntimeConfig: {
+    AUTH_USERNAME: process.env.AUTH_USERNAME,
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+  },
 };
