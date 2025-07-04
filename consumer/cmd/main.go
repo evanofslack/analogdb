186c933d53f9ee74e847929e6ae3269aedf3f5b5
Evan Slack <evan.slack@outlook.com>
2025-07-04 19:45:11 -0400

    logger

--- consumer/cmd/main.go
+++ consumer/cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/evanofslack/analogdb-consumer/internal/config"
+	"github.com/evanofslack/analogdb-consumer/internal/logging"
 )
 
 func main() {
@@ -11,6 +14,17 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to load config:", err)
 	}
-	
-	_ = cfg
+
+	logger, err := logging.New(cfg.Log.Level, cfg.Log.Env, cfg.App.Name)
+	if err != nil {
+		fmt.Printf("Create logger, err=%v\n", err)
+		os.Exit(1)
+	}
+	logger.Info("starting analogdb-consumer",
+		"version", cfg.App.Version,
+		"env", cfg.App.Env,
+	)
+
+	// TODO: use logger in services
+	_ = logger
 }
