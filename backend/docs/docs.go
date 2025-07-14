8411982ecdbfbc385ae3767bf92c969571a891ee
Evan Slack <evan.slack@outlook.com>
2025-07-13 22:46:05 -0400

    swagger

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -1176,13 +1176,13 @@ const docTemplate = `{
                     "type": "boolean",
                     "example": false
                 },
+                "timestamp": {
+                    "type": "integer",
+                    "example": 1752354541
+                },
                 "title": {
                     "type": "string",
                     "example": "A day at the fields [Nikon FM2 | Portra 400]"
-                },
-                "unix_time": {
-                    "type": "integer",
-                    "example": 1752354541
                 }
             }
         },
@@ -1419,13 +1419,13 @@ const docTemplate = `{
                     "type": "boolean",
                     "example": false
                 },
+                "timestamp": {
+                    "type": "integer",
+                    "example": 1752354541
+                },
                 "title": {
                     "type": "string",
                     "example": "A day at the fields [Nikon FM2 | Portra 400]"
-                },
-                "unix_time": {
-                    "type": "integer",
-                    "example": 1752354541
                 }
             }
         },
